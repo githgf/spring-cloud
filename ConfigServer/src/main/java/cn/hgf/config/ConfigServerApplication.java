2bdaa82e428efe964d221da30210c92364debfe4
githgf <hgftest@126.com>
2019-08-19 23:57:36 +0800

    新增部分

--- ConfigServer/src/main/java/cn/hgf/config/ConfigServerApplication.java
+++ ConfigServer/src/main/java/cn/hgf/config/ConfigServerApplication.java
@@ -4,14 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @SpringBootApplication
 @EnableConfigServer
 @EnableEurekaClient
 //configClient 如何调用在client项目中找ConfigServicePropertySourceLocator.getRemoteEnviroment()
-//@EnableZuulProxy
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
--- EurekaClient/src/main/java/cn/hgf/eurekacustomer/EurekaClientApplication.java
+++ EurekaClient/src/main/java/cn/hgf/eurekacustomer/EurekaClientApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 //以下注解作用相同（和@EnableEurekaClient 都是在服务注册中心中注册实例）
